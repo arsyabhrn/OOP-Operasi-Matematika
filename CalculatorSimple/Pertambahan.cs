3de0e897aa6d33b7bd118b973ba488ccf8614de7
Alfatehan Arsya Baharin <alfatehanarsya8998@gmail.com>
2021-04-23 17:29:36 +0700

    Add project files.

---
+++ CalculatorSimple/Pertambahan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorSimple
+{
+    class Pertambahan : Abstract
+    {
+        public int value1 { get; private set; }
+        public int value2 { get; private set; }
+
+        public Pertambahan(int aValue1, int aValue2)
+        {
+            value1 = aValue1;
+            value2 = aValue2;
+        }
+
+        public override int Proccess()
+        {
+            return value1 + value2;
+        }
+    }
+}
---
+++ CalculatorSimple/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pertambahan pertambahan = new Pertambahan(4, 2);
+            Console.WriteLine(pertambahan.Proccess());
+            pertambahan.Display();
+
+            Pengurangan pengurangan = new Pengurangan(4, 2);
+            Console.WriteLine(pengurangan.Proccess());
+            pengurangan.Display();
+
+            Perkalian perkalian = new Perkalian(4, 2);
+            Console.WriteLine(perkalian.Proccess());
+            perkalian.Display();
+
+            Pembagian pembagian = new Pembagian(4, 2);
+            Console.WriteLine(pembagian.Proccess());
+            pembagian.Display();
+        }
+    }
+}
